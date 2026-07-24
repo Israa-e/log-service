3af1032ef15f8807b9ad079bc7df8baf1acdade7
Israa El-halabi <israa00elhalby@gmail.com>
2026-07-24 18:04:02 +0300

    Dockerize the application, connect via DB_HOST/DB_PORT env vars

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,11 +1,9 @@
 import { Pool } from "pg";
 
-export const pool = new Pool(
-    {
-        user: "loguser",
-        password: "logpass",
-        host: "localhost",
-        port: 5433,
-        database: "logdb"
-    }
-);
+export const pool = new Pool({
+  user: "loguser",
+  password: "logpass",
+  host: process.env.DB_HOST || "localhost",
+  port: parseInt(process.env.DB_PORT || "5433", 10),
+  database: "logdb",
+});
